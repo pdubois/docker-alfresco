a01f42f62e958767ba04287169a18caa944a7598
pdubois <philippe.dubois@alfresco.com>
2015-03-06 18:26:24 +0100

    added page len for TrashCleaner

--- tunerepo.sh
+++ tunerepo.sh
@@ -4,6 +4,7 @@ echo "system.content.orphanProtectDays=1" >>  "/opt/alfresco-5.0.c/tomcat/shared
 # configure trashcan cleaner
 echo "trashcan.cleaner.protected.day=1" >>  "/opt/alfresco-5.0.c/tomcat/shared/classes/alfresco-global.properties"
 echo "trashcan.cleaner.cron=0 0 4 * * ?" >> "/opt/alfresco-5.0.c/tomcat/shared/classes/alfresco-global.properties"
+echo "trashcan.cleaner.pagelen=5" >> "/opt/alfresco-5.0.c/tomcat/shared/classes/alfresco-global.properties"
 # disabling deleted content being copied to ContentStore.deleted
 mv /disable-delbackup-context.xml /opt/alfresco-5.0.c/tomcat/shared/classes/alfresco/extension
 
