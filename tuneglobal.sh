59bd4f26fd2b9b8c88738a22f0301c4fbcd92fd9
pdubois <philippe.dubois@alfresco.com>
2015-04-03 21:12:17 +0200

    Add support for .EQ. in order to make it work with tutum

--- tuneglobal.sh
+++ tuneglobal.sh
@@ -2,17 +2,28 @@
 for thing in `env`
 do
  if [[ $thing == ALF* ]]; then
-# getting the value of the parameter  
-     val=`echo "$thing" | awk -F "=" '{print $1}'`
+# replace the "\075" when equal is escaped (see tutum)
+    thingreplaced=`echo -e $thing`
+# another possibility to escape with tutul is to use .EQ. 
+    string_to_replace_with="="
+    result_string="${thingreplaced/.EQ./$string_to_replace_with}"
+    thingreplaced=$result_string
+    echo $thingreplaced
+# getting the value of the parameter
+     val=`echo -e  "$thingreplaced" | awk -F "=" '{print $1}'`
+     echo "val:$val"
 # getting the name value of the configuration variable passed as parameter
-     name=`echo "${!val}" | awk -F "=" '{print $1}'`
-     varvalue=`echo "${!val}" | awk -F "=" '{print $2}'`
+     name=`echo -e "${!val}" | awk -F "\.EQ\.|=" '{print $1}'`
+     echo "name:$name"
+     varvalue=`echo -e "${!val}" | awk -F "\.EQ\.|=" '{print $2}'`
+     echo "varvalue:$varvalue"
 # test if varvalue already configured in alfresco-global.properties
      if grep -q ^$name /opt/alfresco-5.0.c/tomcat/shared/classes/alfresco-global.properties
      then
-        sed -i "/opt/alfresco-5.0.c/tomcat/shared/classes/alfresco-global.properties" -e "s/$name=.*/${!val}/g" 
+        sed -i "/opt/alfresco-5.0.c/tomcat/shared/classes/alfresco-global.properties" -e "s/$name=.*/$name=$varvalue/g"
     else
-        echo ${!val} >> "/opt/alfresco-5.0.c/tomcat/shared/classes/alfresco-global.properties"
+        echo  "$name=$varvalue" >> "/opt/alfresco-5.0.c/tomcat/shared/classes/alfresco-global.properties"
     fi
  fi
 done
+
