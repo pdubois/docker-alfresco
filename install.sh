c90c8d1341ce81ea08d69e8492cf9cbb0d59e45d
pdubois <pdub_3@yahoo.com>
2017-02-13 12:11:24 +0100

    Removed trashcan

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 set -e
 # Moved installation in Dockerfile
 mv /apply_amps_unatended.sh /opt/alfresco/bin
-mv /alfrescotrashcancleaner2.amp /opt/alfresco/amps
 mv /protectnodes.amp /opt/alfresco/amps
 /opt/alfresco/bin/apply_amps_unatended.sh
 mv /custom-surf-application-context.xml /opt/alfresco/tomcat/shared/classes/alfresco/web-extension
